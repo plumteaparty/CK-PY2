2a667493e81e4e187c3cf5197c25a7c5c1893d71
plumteaparty <48882848+plumteaparty@users.noreply.github.com>
2023-01-15 22:02:49 +0300

    Лабораторная работа 2

---
+++ Лабораторная 2/book_class.py
@@ -0,0 +1,48 @@
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+
+# TODO написать класс Book
+class Book:
+    def __init__(self, id_: int, name: str, pages: int):
+        if not isinstance(id_, int):
+            raise TypeError()
+        if id_ < 0:
+            raise ValueError()
+        self.id_ = id_
+        if not isinstance(name, str):
+            raise TypeError()
+            raise ValueError()
+        self.name = name
+        if not isinstance(pages, int):
+            raise TypeError()
+        if pages < 0:
+            raise ValueError()
+        self.pages = pages
+
+    def __str__(self) -> str:
+        return f'Книга "{self.name}"'
+
+    def __repr__(self) -> str:
+        return f'Book(id_={self.id_}, name={self.name!r}, pages={self.pages})'
+
+
+if __name__ == '__main__':
+    # инициализируем список книг
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    for book in list_books:
+        print(book)  # проверяем метод __str__
+
+    print(list_books)  # проверяем метод __repr__
